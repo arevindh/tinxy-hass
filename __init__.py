2a99b7eae61b0b5f6cc40613390ecf27f215e608
itsmesid <693151+arevindh@users.noreply.github.com>
2021-11-14 11:19:33 +0530

    changes

--- __init__.py
+++ __init__.py
@@ -1,61 +1,59 @@
-"""The Detailed Hello World Push integration."""
+"""
+Example of a custom MQTT component.
+Shows how to communicate with MQTT. Follows a topic on MQTT and updates the
+state of an entity to the last message received on that topic.
+Also offers a service 'set_state' that will publish a message on the topic that
+will be passed via MQTT to our message received listener. Call the service with
+example payload {"new_state": "some new state"}.
+Configuration:
+To use the mqtt_example component you will need to add the following to your
+configuration.yaml file.
+mqtt_basic_async:
+  topic: "home-assistant/mqtt_example"
+"""
 from __future__ import annotations
 
-import asyncio
+import voluptuous as vol
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.components import mqtt
+from homeassistant.core import HomeAssistant, ServiceCall, callback
 from homeassistant.helpers.typing import ConfigType
 
-from . import hub
+# The domain of your component. Should be equal to the name of your component.
 from .const import DOMAIN
 
-# List of platforms to support. There should be a matching .py file for each,
-# eg <cover.py> and <sensor.py>
-PLATFORMS: list[str] = ["switch"]
+# Schema to validate API KEY
+CONFIG_SCHEMA = vol.Schema({
+    vol.Required(CONF_API_KEY): cv.string
+})
 
+async def get(url):
+    response = await loop.run_in_executor(executor, requests.get, url)
+    return response.text
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Hello World component."""
-    # Ensure our name space for storing objects is a known type. A dict is
-    # common/preferred as it allows a separate instance of your class for each
-    # instance that has been created in the UI.
-    hass.data.setdefault(DOMAIN, {})
+    """Set up the MQTT async example component."""
+    topic = config[DOMAIN][CONF_TOPIC]
+    entity_id = 'mqtt_example.last_message'
 
+    # Listen to a message on MQTT.
+    @callback
+    def message_received(topic: str, payload: str, qos: int) -> None:
+        """A new MQTT message has been received."""
+        hass.states.async_set(entity_id, payload)
+
+    await hass.components.mqtt.async_subscribe(topic, message_received)
+
+    hass.states.async_set(entity_id, 'No messages')
+
+    # Service to publish a message on MQTT.
+    @callback
+    def set_state_service(call: ServiceCall) -> None:
+        """Service to send a message."""
+        hass.components.mqtt.async_publish(topic, call.data.get('new_state'))
+
+    # Register our service with Home Assistant.
+    hass.services.async_register(DOMAIN, 'set_state', set_state_service)
+
+    # Return boolean to indicate that initialization was successfully.
     return True
-
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Hello World from a config entry."""
-    # Store an instance of the "connecting" class that does the work of speaking
-    # with your actual devices.
-    hass.data[DOMAIN][entry.entry_id] = hub.Hub(hass, entry.data["api_key"])
-
-    # This creates each HA object for each platform your device requires.
-    # It's done by calling the `async_setup_entry` function in each platform module.
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
-
-    return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    # This is called when an entry/configured device is to be removed. The class
-    # needs to unload itself, and remove callbacks. See the classes for further
-    # details
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(
-                    entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
